2b00d42b7bf444d5f7018e79c26d60c22a2e289f
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-01-11 18:44:52 +0530

    Updated info updation file and removed solana specific code

--- utils/update_info.js
+++ utils/update_info.js
@@ -20,15 +20,9 @@ let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
 let data = JSON.parse(rawdata);
 
 data.forEach((item) => {
-  if (network == NETWORK.sol) {
-    item.name = `${namePrefix} #${item.edition}`;
-    item.description = description;
-    item.creators = solanaMetadata.creators;
-  } else {
-    item.name = `${namePrefix} #${item.edition}`;
-    item.description = description;
-    item.image = `${baseUri}/${item.edition}.png`;
-  }
+  item.name = `${namePrefix} #${item.edition}`;
+  item.description = description;
+  item.image = `${baseUri}/${item.edition}.png`;
   fs.writeFileSync(
     `${basePath}/build/json/${item.edition}.json`,
     JSON.stringify(item, null, 2)
@@ -40,16 +34,7 @@ fs.writeFileSync(
   JSON.stringify(data, null, 2)
 );
 
-if (network == NETWORK.sol) {
-  console.log(`Updated description for images to ===> ${description}`);
-  console.log(`Updated name prefix for images to ===> ${namePrefix}`);
-  console.log(
-    `Updated creators for images to ===> ${JSON.stringify(
-      solanaMetadata.creators
-    )}`
-  );
-} else {
-  console.log(`Updated baseUri for images to ===> ${baseUri}`);
-  console.log(`Updated description for images to ===> ${description}`);
-  console.log(`Updated name prefix for images to ===> ${namePrefix}`);
-}
+console.log(`Updated baseUri for images to ===> ${baseUri}`);
+console.log(`Updated description for images to ===> ${description}`);
+console.log(`Updated name prefix for images to ===> ${namePrefix}`);
+
