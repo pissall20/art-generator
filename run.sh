22f6648d368e5f8d14da04703b193e44d26be1fe
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-01-12 18:21:52 +0530

    Added a shell script for single entry point

---
+++ run.sh
@@ -0,0 +1,17 @@
+# run.sh - Script to start the generation process
+
+node_max_memory=40960
+
+# Set node max memory to a large number (highest possible, based on available RAM)
+export NODE_OPTIONS=--max_old_space_size=$node_max_memory
+
+# Start the generation process
+# Install required packages
+npm install
+# Install required python dependencies
+pip install pandas
+# Start the generation process. Save the screen output to a file
+npm run build > npm_generation.log
+# Run python program to create rarity and occurences for NFT partner
+python utils/occurences.py
+
