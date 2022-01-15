deacd01fb4057a698ba8a6259c14d1e1c8928365
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-01-15 10:56:39 +0530

    Updated config file

--- src/config.js
+++ src/config.js
@@ -8,37 +8,30 @@ const { NETWORK } = require(path.join(basePath, "constants/network.js"));
 
 const network = NETWORK.eth;
 
-// General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
-const baseUri = "ipfs://NewUriToReplace";
+// General metadata 
+const namePrefix = "Zombivali Collection 1";
+const description = "Zombivali - Sa Re Ga Ma";
+const baseUri = "https://www.rpsg.in";
 
-const solanaMetadata = {
-  symbol: "YC",
-  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
-  creators: [
-    {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
-      share: 100,
-    },
-  ],
-};
 
-// If you have selected Solana then the collection starts from 0 automatically
+// Collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 40,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "1. background" },
+      { name: "2. base" },
+      { name: "3. skin" },
+      { name: "4. headgear" },
+      { name: "5. ear" },
+      { name: "6. eyes" },
+      { name: "7. nose" },
+      { name: "8. mouth" },
+      { name: "9. cloth" },
+      { name: "10. neck gear" },
+      { name: "11. hand gears" },
     ],
-  },
+  }
 ];
 
 const shuffleLayerConfigurations = false;
@@ -46,12 +39,12 @@ const shuffleLayerConfigurations = false;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 2000,
+  height: 2000,
 };
 
 const gif = {
-  export: false,
+  export: true,
   repeat: 0,
   quality: 100,
   delay: 500,
@@ -81,7 +74,9 @@ const background = {
   default: "#000000",
 };
 
-const extraMetadata = {};
+const extraMetadata = {
+  creator: "Mooz.One, NME, 404",
+};
 
 const rarityDelimiter = "#";
 
@@ -119,7 +114,6 @@ module.exports = {
   text,
   namePrefix,
   network,
-  solanaMetadata,
   gif,
   preview_gif,
 };
